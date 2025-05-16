ccfc2630f734b2e46d66f07e82283ee0aa673a03
Adyanullah <151485379+Adyanullah@users.noreply.github.com>
2025-05-16 11:31:58 +0700

    add require message on form tambah data

--- resources/views/inventaris/app.blade.php
+++ resources/views/inventaris/app.blade.php
@@ -2,9 +2,9 @@
     <button type="button" class="btn btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#TambahData">
         Tambah Data
     </button>
-    @if ($errors->any())
+    {{-- @if ($errors->any())
         @dd($errors->all())
-    @endif
+    @endif --}}
     <div class="modal fade" id="TambahData" tabindex="-1" aria-labelledby="exampleModalCenteredScrollableTitle"
         aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
@@ -19,18 +19,30 @@
                         <label for="nama_barang" class="form-label">Nama Barang</label>
                         <input type="text" class="form-control" id="nama_barang" name="nama_barang" value="{{ old('nama_barang') }}">
                     </div>
+                    @error('nama_barang')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <div class="mb-3">
                         <label for="jenis_barang" class="form-label">Jenis Barang</label>
                         <input type="text" class="form-control" id="jenis_barang" name="jenis_barang" value="{{ old('jenis_barang') }}">
                     </div>
+                    @error('jenis_barang')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <div class="mb-3">
                         <label for="merk_barang" class="form-label">Merk/Spesifikasi</label>
                         <input type="text" class="form-control" id="merk_barang" name="merk_barang" value="{{ old('merk_barang') }}">
                     </div>
+                    @error('merk_barang')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <div class="mb-3">
                         <label for="tahun_perolehan" class="form-label">Tahun Perolehan</label>
                         <input type="number" min="1900" max="2099" step="1" class="form-control" id="tahun_perolehan" name="tahun_perolehan" value="{{ old('tahun_perolehan') }}">
                     </div>
+                    @error('tahun_perolehan')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <div class="mb-3">
                         <label for="sumber_dana" class="form-label">Sumber Dana</label>
                         <select class="form-select" id="sumber_dana" name="sumber_dana" >
@@ -52,6 +64,9 @@
                         <label for="jumlah_barang" class="form-label">Jumlah Barang</label>
                         <input type="number" class="form-control" id="jumlah_barang" name="jumlah_barang" value="{{ old('jumlah_barang') }}">
                     </div>
+                    @error('jumlah_barang')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <div class="mb-3">
                         <label for="ruangan_id" class="form-label">Lokasi</label>
                         <select class="form-select" id="ruangan_id" name="ruangan_id">
@@ -61,6 +76,9 @@
                             @endforeach
                         </select>
                     </div>
+                    @error('ruangan_id')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <div class="mb-3">
                         <label for="kondisi" class="form-label">Kondisi</label>
                         <select class="form-select" id="kondisi" name="kondisi">
@@ -79,6 +97,9 @@
                             <option value="pinjam">Pinjam</option>
                         </select>
                     </div>
+                    @error('nama_barang')
+                        <div class="alert alert-danger mt-2">{{ $message }}</div>
+                    @enderror
                     <div class="mb-3">
                         <label for="penanggung_jawab" class="form-label">Penanggung Jawab</label>
                         <input type="text" class="form-control" id="penanggung_jawab" name="penanggung_jawab" value="{{ old('penanggung_jawab') }}">
