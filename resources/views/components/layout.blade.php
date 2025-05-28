e70a45b8755df14ce6ff89a85fe8de107c089aed
iqbilqauly <iqbilqauly17@gmail.com>
2025-05-28 10:35:35 +0700

    dropdown

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -19,7 +19,12 @@
     <link href="{{ asset('/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
     <script src="{{ asset('/assets/js/config.min.js') }}"></script>
-    <!-- Di akhir file blade kamu, sebelum </body> -->
+    <style>
+        .dropdown-item.text-white:hover {
+            background-color:rgb(176, 176, 176) !important;
+            color: white !important;
+        }
+    </style>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
             document.querySelectorAll('.modal').forEach(modal => {
