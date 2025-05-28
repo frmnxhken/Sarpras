b6b11f6d3768873061c57863c18095094b22b8a5
iqbilqauly <iqbilqauly17@gmail.com>
2025-05-28 12:09:12 +0700

    ganti judul detail pengadaan

--- resources/views/pengadaan/detail.blade.php
+++ resources/views/pengadaan/detail.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalDetailPengadaanLabel">Detail Pengadaan</h5>
+                <h5 class="modal-title" id="modalDetailPengadaanLabel">Detail Barang</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body">
